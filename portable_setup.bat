3bf8c1ec98b8ce24076a0d3b944b5eb385268825
hadron13 <62845905+hadron13@users.noreply.github.com>
2025-10-23 11:39:47 -0300

    Fix portable setup

--- portable_setup.bat
+++ portable_setup.bat
@@ -7,7 +7,7 @@ if not exist cimgui\Makefile (
 )
 
 if not exist w64devkit\ (
-    curl -L https://github.com/skeeto/w64devkit/releases/download/v2.4.0/w64devkit-x86-2.4.0.7z.exe --ssl-no-revoke > w64devkit.exe
+    curl -L https://github.com/skeeto/w64devkit/releases/download/v2.4.0/w64devkit-x64-2.4.0.7z.exe --ssl-no-revoke > w64devkit.exe
     if exist w64devkit.exe (
         .\w64devkit.exe
         del w64devkit.exe
@@ -15,9 +15,8 @@ if not exist w64devkit\ (
 )
 
 if not exist sdl3\ (
-    curl -L https://github.com/libsdl-org/SDL/releases/download/release-3.2.24/SDL3-devel-3.2.24-mingw.zip--ssl-no-revoke > libsdl3.zip
+    curl -L https://github.com/libsdl-org/SDL/releases/download/release-3.2.24/SDL3-devel-3.2.24-mingw.zip --ssl-no-revoke > libsdl3.zip
     powershell -command "Expand-Archive -Path 'libsdl3.zip' -DestinationPath '.\sdl3'"
     move sdl3\SDL3-3.2.24\x86_64-w64-mingw32\bin\SDL3.dll build
     del libsdl3.zip 
 )
-
