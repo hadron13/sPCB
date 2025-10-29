50b9dc119f3b010d0008ce179ced634c3ff4aa24
aluno <carlosbpfonseca@gmail.com>
2025-10-29 14:39:26 -0300

    update windows build script

--- build.bat
+++ build.bat
@@ -1,20 +1,23 @@
 @echo off
 setlocal
 set PATH=.\w64devkit\bin;%PATH%
-set PATH=.\w64devkit\bin;%PATH%
+set PATH=..\w64devkit\bin;%PATH%
 
 mkdir build\libs
 
 set SDL3_DIR=sdl3/SDL3-3.2.24/x86_64-w64-mingw32
 
-@REM if not exist build\libs\libcimgui.a (
-@REM     cd cimgui
-@REM     make static
-@REM     cd ..
-@REM     move cimgui\libcimgui.a build\libs\
-@REM )
-@REM if not exist build\libs\cimgui_impl.o (
-@REM     g++ src\cimgui_sdl2_renderer\cimgui_impl.cpp -fPIC -c -Icimgui/imgui -o build\libs\cimgui_impl.o -Isdl2\SDL2-2.32.10\x86_64-w64-mingw32\include\SDL2
-@REM )
+if not exist build\libs\libcimgui.a (
+    cd cimgui
+    make static
+    cd ..
+    move cimgui\libcimgui.a build\libs\
+)
+if not exist build\libs\cimgui_impl.o (
+    g++ cimgui\cimgui_impl.cpp -DCIMGUI_USE_SDL3 -DCIMGUI_USE_OPENGL3 -DIMGUI_IMPL_API="extern \"C\" __declspec(dllexport)" -fPIC -c -Icimgui/imgui/backends -Icimgui/imgui -o build\libs\cimgui_impl.o 
+)
+if not exist build\libs\glad.o (
+    gcc glad/src/gl.c -c -o build/libs/glad.o -I glad/include
+)
 
-gcc src/*.c glad/src/gl.c -o build/spcb.exe -lSDL3 -mwindows -Iglad/include -I%SDL3_DIR%/include -Lbuild
+gcc src/*.c build/libs/libcimgui.a build/libs/cimgui_impl.o build/libs/glad.o -o build/spcb.exe -lSDL3 -mwindows -Iglad/include -I%SDL3_DIR%/include -Lbuild
