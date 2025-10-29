c07f2fcd6a9f859590671bc28ab4f0ca0d2e347a
aluno <carlosbpfonseca@gmail.com>
2025-10-29 15:39:44 -0300

    in process of adding Dear Imgui

--- build.bat
+++ build.bat
@@ -14,10 +14,10 @@ if not exist build\libs\libcimgui.a (
     move cimgui\libcimgui.a build\libs\
 )
 if not exist build\libs\cimgui_impl.o (
-    g++ cimgui\cimgui_impl.cpp -DCIMGUI_USE_SDL3 -DCIMGUI_USE_OPENGL3 -DIMGUI_IMPL_API="extern \"C\" __declspec(dllexport)" -fPIC -c -Icimgui/imgui/backends -Icimgui/imgui -o build\libs\cimgui_impl.o 
+    @REM g++ cimgui\imgui\backends\imgui_impl_opengl3.cpp cimgui\imgui\backends\imgui_impl_opengl3.cpp cimgui\imgui\backends\imgui_impl_sdl3.cpp -DCIMGUI_USE_SDL3 -DCIMGUI_USE_OPENGL3 -DIMGUI_IMPL_API="extern \"C\" __declspec(dllexport)" -fPIC -lSDL3 -Icimgui/imgui/backends -I%SDL3_DIR%/include -Icimgui/imgui -o build\libs\cimgui_impl.o 
 )
 if not exist build\libs\glad.o (
     gcc glad/src/gl.c -c -o build/libs/glad.o -I glad/include
 )
 
-gcc src/*.c build/libs/libcimgui.a build/libs/cimgui_impl.o build/libs/glad.o -o build/spcb.exe -lSDL3 -mwindows -Iglad/include -I%SDL3_DIR%/include -Lbuild
+gcc src/*.c build/libs/libcimgui.a build/libs/cimgui_impl.o build/libs/glad.o -o build/spcb.exe -lSDL3 -mwindows -Iglad/include -I%SDL3_DIR%/include -I. -Lbuild -lstdc++
--- src/main.c
+++ src/main.c
@@ -2,6 +2,13 @@
 #include<SDL3/SDL.h>
 #include<glad/gl.h>
 
+#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include<cimgui/cimgui.h>
+
+#define CIMGUI_USE_SDL3
+#define CIMGUI_USE_OPENGL3
+#include<cimgui/cimgui_impl.h>
+
 
 void simulation_init();
 void simulation_step();
@@ -12,6 +19,8 @@ int main(){
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    
+    float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
 
     SDL_Window * window = SDL_CreateWindow("title", 800, 600, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
     
@@ -36,27 +45,59 @@ int main(){
 
     SDL_Log("loaded OpenGL version %s", glGetString(GL_VERSION));
     SDL_Log("vendor: %s", glGetString(GL_VENDOR)); 
+ 
+    igCreateContext(NULL);
+    ImGuiIO* io = igGetIO_Nil(); (void)io;
+    io->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     
+
+    // Setup Dear ImGui style
+    igStyleColorsDark(NULL);
+    //igStyleColorsLight(NULL);
+    // Setup scaling
+    ImGuiStyle* style = igGetStyle();
+    ImGuiStyle_ScaleAllSizes(style, main_scale);     
+    style->FontScaleDpi = main_scale;        
+    io->ConfigDpiScaleFonts = true;          
+    io->ConfigDpiScaleViewports = true;      
+
+    ImGui_ImplSDL3_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init("#version 150");
+
+
+    igStyleColorsDark(NULL);
 
     simulation_init();
 
     bool running = true;
+    bool show_demo_window = true;
     while(running){
 
         SDL_Event event;
         while(SDL_PollEvent(&event)){
+            ImGui_ImplSDL3_ProcessEvent(&event);
             if(event.type == SDL_EVENT_QUIT){
                 running = false;
             }
         }
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        igNewFrame();
 
         glClearColor(0.0, 0.0, 0.0, 1.0); 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         simulation_step();
-        SDL_Delay(700);
 
+        if (show_demo_window)
+            igShowDemoWindow(&show_demo_window);
+        // SDL_Delay(700);
+        ImGui_ImplOpenGL3_RenderDrawData(igGetDrawData());
+        
         SDL_GL_SwapWindow(window);
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
 
     SDL_GL_DestroyContext(gl_context);
 
